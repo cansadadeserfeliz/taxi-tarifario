d652f259f1bd0b63f947acf2c6ddafb1e7971f1e
Vera Mazhuga <vero4ka.ru@gmail.com>
2016-01-27 10:25:37 +0100

    Add select for neighborhoods

--- js/app.js
+++ js/app.js
@@ -3,6 +3,24 @@
   
   app.controller('RateCalculatorController', function(){
     this.price = 0;
+    this.neighborhoods = [
+      {
+        name: 'Barrancas',
+        id: 1
+      }, {
+        name: 'Chico',
+        id: 2
+      }, {
+        name: 'San Cristobal',
+        id: 3
+      }, {
+        name: 'Mandalay',
+        id: 4
+      }, {
+        name: 'Kennedy',
+        id: 5
+      }
+    ]
     
     this.getPrice = function() {
       this.price = 200;
