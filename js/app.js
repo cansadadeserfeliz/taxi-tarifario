31f3f682f8f61601b15bd351b792acabc69c5b49
Vera Mazhuga <vero4ka.ru@gmail.com>
2016-01-27 11:22:10 +0100

    Add validation for equal ofigin and destination

--- js/app.js
+++ js/app.js
@@ -1,26 +1,33 @@
 (function(){
   var app = angular.module('tarifario', []);
   
-  app.controller('RateCalculatorController', function(){
-    this.price = 0;
-    this.neighborhoods = [
+  app.controller('RateCalculatorController', [ '$http', function($http){
+    var rateCtrl = this;
+    rateCtrl.price = 0;
+    rateCtrl.neighborhoods = []
+    
+    //$http.get('locations.json').success(function(data){
+    //  rateCtrl.neighborhoods = data;
+    //});
+    
+    rateCtrl.neighborhoods = [
       {
-        name: 'Barrancas',
-        id: 1
+        "name": "Barrancas",
+        "id": 1
       }, {
-        name: 'Chico',
-        id: 2
+        "name": "Chico",
+        "id": 2
       }, {
-        name: 'San Cristobal',
-        id: 3
+        "name": "San Cristobal",
+        "id": 3
       }, {
-        name: 'Mandalay',
-        id: 4
+        "name": "Mandalay",
+        "id": 4
       }, {
-        name: 'Kennedy',
-        id: 5
+        "name": "Kennedy",
+        "id": 5
       }
-    ]
+    ];
     
     this.getPrice = function() {
       this.price = 200;
@@ -30,5 +37,31 @@
       this.origin = '';
       this.destination = '';
     }
-  });
+  } ]);
+  
+  app.directive('equalsToOrigin', [
+    function() {
+      var link = function($scope, $element, $attrs, ctrl) {
+        var validate = function(viewValue) {
+          var comparisonModel = $attrs.equalsToOrigin;
+          if(!viewValue || !comparisonModel){
+            // It's valid because we have nothing to compare against
+            ctrl.$setValidity('equalsToOrigin', true);
+          }
+          // It's valid if model is lower than the model we're comparing against
+          ctrl.$setValidity('equalsToOrigin', parseInt(viewValue, 10) != parseInt(comparisonModel, 10) );
+          return viewValue;
+        };
+        ctrl.$parsers.unshift(validate);
+        ctrl.$formatters.push(validate);
+        $attrs.$observe('equalsToOrigin', function(comparisonModel){
+          return validate(ctrl.$viewValue);
+        });
+      };
+      return {
+        require: 'ngModel',
+        link: link
+      };
+    }
+  ]);
 })();
